801c946d894dab88abe8c3636acf56a6610f3e92
emtkala <emtkala@gmail.com>
2021-08-14 20:53:50 +0300

    LIST

---
+++ LIST.py
@@ -0,0 +1,17 @@
+
+lucky_numbers = [4, 1, 2, 10, 330, 100]
+friends = ["Joe", "Jeff", "Greg", "George", "George", "George", "Daniel"]
+
+friends.append("Jack")
+friends.insert(1, "James")
+friends.remove("Greg")
+friends.pop()
+friends.sort()
+
+friends2 = friends.copy()
+print(friends2)
+friends.reverse()
+
+lucky_numbers.reverse()
+print(lucky_numbers)
+print(friends)
